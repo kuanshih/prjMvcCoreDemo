9f42542367edc821fbf7cf0e7b613ebca768a5c8
kuan <kuanshih2023@gmail.com>
2023-08-07 23:47:04 +0800

    update

--- prjMvcCoreDemo/Models/TProduct.cs
+++ prjMvcCoreDemo/Models/TProduct.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 
 namespace prjMvcCoreDemo.Models;
 
 public partial class TProduct
 {
     public int FId { get; set; }
-    [DisplayName("商品名稱")]
+
     public string? FName { get; set; }
-    [DisplayName("商品庫存")]
+
     public int? FQty { get; set; }
-    [DisplayName("商品成本")]
+
     public decimal? FCost { get; set; }
-    [DisplayName("商品單價")]
+
     public decimal? FPrice { get; set; }
 
-    [DisplayName("產品照片")]
     public string? FImagePath { get; set; }
 }
